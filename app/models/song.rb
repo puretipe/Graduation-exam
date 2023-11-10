ca3aae892dd60ac44b5dd7d40819217de81a8778
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2023-11-10 19:58:10 +0900

    評価機能の実装

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   belongs_to :focus_point
+  has_many :evaluations, dependent: :destroy
   attr_accessor :genre_name
 
   before_validation :set_genre, on: :create
