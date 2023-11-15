5ac6335067da6dae2d14ab12e562c40bce76bf1c
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2023-11-15 21:48:01 +0900

    bootstrap再適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'songs/index'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
