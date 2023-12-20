162b9d8ff29688b46ab0772894d698c1e2cdc399
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2023-12-20 12:08:53 +0900

    お気に入り一覧表示機能

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :require_login, only: [:new, :create, :my_songs, :edit, :update, :destroy]
+  before_action :require_login, only: [:new, :create, :my_songs, :edit, :update, :destroy, :favorites]
 
   def index
     @q = Song.ransack(params[:q])
@@ -80,6 +80,19 @@ class SongsController < ApplicationController
 
   def my_songs
     @q = current_user.songs.ransack(params[:q])
+    @songs = @q.result.includes(:genre, :focus_point, :evaluations)
+    if params[:q] && params[:q][:s] && params[:q][:s].include?("_evaluations_count")
+      sort_param = params[:q][:s]
+      evaluation_type, order = sort_param.split(' ').first.split('_evaluations_count').first, sort_param.split.last
+      @songs = @songs.sorted_by_evaluations(evaluation_type, order)
+    else
+      @songs = @songs.order(created_at: :desc)
+    end
+    @songs = @songs.page(params[:page])
+  end
+
+  def favorites
+    @q = current_user.favorited_songs.ransack(params[:q])
     @songs = @q.result.includes(:genre, :focus_point, :evaluations)
     if params[:q] && params[:q][:s] && params[:q][:s].include?("_evaluations_count")
       sort_param = params[:q][:s]
