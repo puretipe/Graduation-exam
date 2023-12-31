50c2d32d5192b3d27f1238d0b46901b5487e4b86
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2023-12-31 18:49:34 +0900

    修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  def reset_password_email(user)
-    @user = user
-    mail(to: user.email, from: 'puretipe1104@gmail.com', subject: 'パスワードリセットの手順')
-  end
+  default from: "puretipe1104@gmail.com"
+  layout "mailer"
 end
