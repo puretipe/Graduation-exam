5fb64692b90ca5b14986e78953dd969fa9627713
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2024-01-08 23:16:49 +0900

    修正

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -3,12 +3,10 @@ class AutocompletesController < ApplicationController
     query = params[:term].downcase
     results = Song.joins(:genre).where('lower(songs.title) LIKE ? OR lower(songs.artist) LIKE ? OR lower(songs.software_name) LIKE ? OR lower(genres.name) LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
 
-    # クエリに部分一致する結果のみをフィルタリングする
     filtered_results = results.select do |song|
       query.in?(song.title.downcase) || query.in?(song.artist.downcase) || query.in?(song.software_name.downcase) || query.in?(song.genre.name.downcase)
     end
 
-    # 適切なフォーマットを選択し、ユニークな結果のみを返す
     formatted_results = filtered_results.map do |song|
       if song.title.downcase.include?(query)
         song.title
