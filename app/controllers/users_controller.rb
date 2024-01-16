7da5b82e782fe39b985b24f4a5b256e297643b65
比嘉純渓 <hikajunkei@hikajunkeinoMacBook-Air.local>
2024-01-16 18:43:14 +0900

    新規登録時自動ログインするように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def create        
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       flash[:success] = '登録が完了しました'
-      redirect_to new_user_session_path
+      redirect_to root_path
     else
       flash.now[:danger] = '登録に失敗しました'
       render :new, status: :unprocessable_entity
